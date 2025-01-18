a9a26ce7d945a46b2d9db1a1dcf1da99d6ea4c1c
Елена <pox4not@gmail.com>
2025-01-18 11:43:56 +0300

    создание бд

--- module14/crud_functions.py
+++ module14/crud_functions.py
@@ -1,29 +1,49 @@
 import sqlite3
 
+
 def initiate_db():
-    connection = sqlite3.connect('products.db')
-    cursor = connection.cursor()
-
-
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Products(
-    id INTEGER PRIMARY KEY,
-    title TEXT NOT NULL,
-    description TEXT,
-    price INTEGER NOT NULL
-    );
-    ''')
+    with sqlite3.connect('products.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Products (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                description TEXT NOT NULL,
+                price INTEGER NOT NULL
+            )
+        ''')
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL,
+                email TEXT NOT NULL,
+                age INTEGER NOT NULL,
+                balance INTEGER NOT NULL DEFAULT 1000
+            )
+        ''')
+        conn.commit()
 
 def get_all_products():
-
     connection = sqlite3.connect('products.db')
     cursor = connection.cursor()
-
     cursor.execute("SELECT title, description, price FROM Products")
-    users = cursor.fetchall()
-
+    products = cursor.fetchall()
     connection.commit()
-    return users
+    return products
 
+def add_user(username, email, age):
+    connection = sqlite3.connect('products.db')
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES(?, ?, ?, ?)", (username, email, age, 1000))
+    connection.commit()
+    return cursor.lastrowid
+
+def is_included(username):
+    connection = sqlite3.connect('products.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT username FROM Users WHERE username = ?", (username,))
+    result = cursor.fetchone()
+    connection.commit()
+    return result is not None
 
 
